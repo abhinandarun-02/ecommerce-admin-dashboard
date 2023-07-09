8ca404fb6a679fdd307e4f8a0e7f5e687330e157
Abhinand Arun <abhinandarun2015@gmail.com>
2023-07-09 21:46:39 +0530

    Modal Components

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { Modal } from "@/components/ui/modal";
+
+export const StoreModal = () => {
+    
+    const StoreModal = useStoreModal();
+
+  return (
+    <Modal 
+    title="Test"
+    description="Test Description"
+    isOpen={StoreModal.isOpen}
+    onClose={StoreModal.onClose}
+    >Future Create Store Form</Modal>
+  )
+}
